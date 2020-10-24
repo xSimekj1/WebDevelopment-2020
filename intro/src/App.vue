9b116aff91a7437046b27de38e33b5d74d8e5e22
Jakub Simek <Jakapo.simek95@gmail.com>
2020-10-24 22:14:13 +0200

    [kurz-1] create example projet

--- intro/src/App.vue
+++ intro/src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <!-- Klasicky cyklus, pouzity priamo v HTML template, dynamicky generuje tvoju strukturu -->
+        <div
+          v-for="pizza in pizzas"
+          :key="pizza.id"
+          class="col col-1-3"
+        >
+        <span v-text="'<pizza-card>'"></span>
+        <div class="card-wrapper">
+          <pizza-card
+            :pizza="pizza"
+          />
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import data from './assets/data.json';
+import PizzaCard from './components/PizzaCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    PizzaCard,
+  },
+  data() {
+    return {
+      pizzas: data.pizza,
+    };
   },
 };
 </script>
 
 <style lang="less">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +46,72 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style-type: none;
+}
+
+li {
+  text-decoration: none;
+}
+
+img {
+  width: 100%;
+  height: auto;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+// Ukazka jednoduchej grid struktury
+.col {
+  display: flex;
+  flex-direction: column;
+
+  &-1-1 {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+
+  &-1-2 {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+
+  &-1-3 {
+    flex: 0 0 33%;
+    max-width: 33%;
+  }
+
+  &-1-4 {
+    flex: 0 0 25%;
+    max-width: 25%;
+  }
+
+  // Repsonzivita pre tablety a mobilne zariadenia
+  @media only screen and (max-width: 768px) {
+    max-width: 50%;
+    flex: 0 0 50%;
+  }
+
+  @media only screen and (max-width: 450px) {
+    max-width: 100%;
+    flex: 0 0 100%;
+  }
+}
+
+.card-holder {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card-wrapper {
+  display: flex;
+  align-items: stretch;
+  flex: 1 1 auto;
+  margin: 10px;
 }
 </style>
---
+++ intro/src/components/PizzaCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- BEM: pozri aj trello, sposob ako vytvaras jednoducho prepouzitelne komponenty -->
+  <!-- CSS kopiruje strukturu dat -->
+  <div class="pizza-card">
+    <div class="pizza-card__image-wrapper">
+      <img src="../assets/pizza.jpg" alt="pizza image" class="pizza-card__image">
+    </div>
+    <div class="pizza-card__body">
+      <h2
+        class="pizza-card__title"
+        v-text="pizza.name"
+      >
+      </h2>
+      <div class="pizza-card__description">
+        <ul class="pizza-card__list">
+          <!-- Klasicky cyklus, pouzity priamo v HTML template -->
+          <!-- dynamicky generuje tvoju strukturu -->
+          <li
+            v-for="(ingedient, index) in pizza.description"
+            :key="index"
+            class="pizza-card__list-item"
+          >
+            {{ ingedient }}
+          </li>
+        </ul>
+      </div>
+      <div class="pizza-card__bottom">
+        <!-- String literal: formatuje text s premennymi do spolocneho vystupneho retazca-->
+        <span v-text="`Cena: ${pizza.price}€`"></span>
+        <!-- BAD PRACTICE: manualne spajanie stringov-->
+        <!-- <span v-text="'Cena:' + pizza.price + '€'"></span> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PizzaCard',
+  props: {
+    pizza: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.pizza-card {
+  padding: 16px;
+
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px 0px rgba(0,0,0,0.75);
+}
+
+.pizza-card__title {
+  color: #FFA69E;
+  border-bottom: 1px solid #FFA69E;
+  margin: 8px;
+  padding-bottom: 16px;
+}
+
+.pizza-card__list {
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.pizza-card__list-item {
+  margin: 4px;
+  padding: 5px 10px;
+
+  color: #ffffff;
+  border: 1px solid #FFA69E;
+  background: #FFA69E;
+  border-radius: 15px;
+}
+</style>
