929d50184f05cf85d7429c8789bb4bcfc6254fa6
Jakub Simek <Jakapo.simek95@gmail.com>
2020-11-08 18:13:36 +0100

    kurz/3 ADD Less-loader

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <simple-form msg="Account succesfuly created!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleForm from './components/SimpleForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SimpleForm
   }
 }
 </script>
---
+++ forms/src/components/SimpleForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="simple-form">
+    <h1 v-if="created">{{ msg }}</h1>
+    <form action="submit" class="simple-form__form">
+      <div class="simple-form__item">
+        <input
+            ref="email"
+            v-model="email"
+            type="email"
+            placeholder="email..."
+        >
+      </div>
+      <div class="simple-form__item">
+        <input
+            ref="password"
+            v-model="password"
+            type="password"
+            placeholder="heslo..."
+        >
+      </div>
+      <div>
+        <button>Skontroluj</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimpleForm',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      created: null,
+    }
+  },
+  methods: {
+    submit() {
+      console.log("Hello Svete")
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
